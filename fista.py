3a5ec295d7b013f5057b91b4afc878802f37be10
Fergal Cotter <fbcotter90@gmail.com>
2018-08-15 18:26:07 +0100

    Made code python3 runnable from command line
    Also allowed training on your own images by adding in the prefiltering
    functions.

--- fista.py
+++ fista.py
@@ -1,18 +1,17 @@
 import numpy as np
 import math
-import scipy.sparse as sps
 import scipy.sparse.linalg
-import time
+
 
 def fista(I, Phi, lambdav, max_iterations=150, display=False):
-  """ FISTA Inference for Lasso (l1) Problem 
+  """ FISTA Inference for Lasso (l1) Problem
   I: Batches of images (dim x batch)
   Phi: Dictionary (dim x dictionary element) (nparray or sparse array)
   lambdav: Sparsity penalty
   max_iterations: Maximum number of iterations
   """
   def proxOp(x,t):
-    """ L1 Proximal Operator """ 
+    """ L1 Proximal Operator """
     return np.fmax(x-t, 0) + np.fmin(x+t, 0)
 
   x = np.zeros((Phi.shape[1], I.shape[1]))
@@ -32,7 +31,8 @@ def fista(I, Phi, lambdav, max_iterations=150, display=False):
     y = x2 + ((t-1)/t2)*(x2-x)
     x = x2
     t = t2
-    if display == True:
-      print "L1 Objective " +  str(np.sum((I-Phi.dot(x2))**2) + lambdav*np.sum(np.abs(x2)))
+    if display:
+      print("L1 Objective {}".format(
+          np.sum((I-Phi.dot(x2))**2) + lambdav*np.sum(np.abs(x2))))
 
   return x2
--- sparsenet.py
+++ sparsenet.py
@@ -4,42 +4,63 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import fista
+import argparse
+import os
+from PIL import Image
+from numpy.fft import fft2, ifft2, fftshift
 
-def sparsenet(patch_dim=64, neurons=128, lambdav=0.1, eta=6.0, num_trials=3000, batch_size=100, border=4, inference='lca'):
+parser = argparse.ArgumentParser(description='Compute Overcomplete Sparse '
+                                 'Dictionary')
+parser.add_argument('--imgs', dest='imgs', type=str, default=None,
+                    help='Path to folder of images (not prescaled).')
+parser.add_argument('--lambdav', type=float, default=0.1,
+                    help='Image variance after whitening')
+parser.add_argument('--inference', type=str, default='fista',
+                    choices=['fista', 'lca'],
+                    help='The inference algorithm used')
+parser.add_argument('--size', type=int, default=8,
+                    help='Patch side length')
+parser.add_argument('--suffix', dest='suffix', nargs='+',
+                    default=['.jpeg', '.jpg', '.png'],
+                    help='List of acceptable suffixes to try load in the image '
+                    'folder (should include the ".")')
+
+
+def sparsenet(IMAGES, patch_dim=8, neurons=128, lambdav=0.1, eta=6.0,
+              num_trials=3000, batch_size=100, border=4, inference='lca'):
   """
-  N: # Inputs
-  M: # Outputs
+  IMAGES: list or array of images. First dimension should be the number of
+    images. If list, the images can be of different size.
+  patch_dim - side length of patch
   lambdav: Sparsity Constraint
   eta: Learning Rate
   num_trials: Learning Iterations
   batch_size: Batch size per iteration
   border: Border when extracting image patches
-  Inference: 'lca' or 'fista'  
+  Inference: 'lca' or 'fista'
   """
-  IMAGES = scipy.io.loadmat('./IMAGES.mat')
-  IMAGES = IMAGES['IMAGES']
+  num_images = len(IMAGES)
 
-  (imsize, imsize, num_images) = np.shape(IMAGES)
-
-  sz = np.sqrt(patch_dim)
+  sz = patch_dim
   eta = eta / batch_size
 
   # Initialize basis functions
-  Phi = np.random.randn(patch_dim, neurons)
-  Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis = 0))))
+  Phi = np.random.randn(sz*sz, neurons)
+  Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis=0))))
 
-  I = np.zeros((patch_dim,batch_size))
+  I = np.zeros((sz*sz,batch_size))
 
   for t in range(num_trials):
 
     # Choose a random image
-    imi = np.ceil(num_images * random.uniform(0, 1))
+    imi = np.random.randint(num_images)
+    s1, s2 = IMAGES[imi].shape
 
     for i in range(batch_size):
-      r = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
-      c = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
+      r = border + np.random.randint(s1-sz-2*border)
+      c = border + np.random.randint(s2-sz-2*border)
 
-      I[:,i] = np.reshape(IMAGES[r:r+sz, c:c+sz, imi-1], patch_dim, 1)
+      I[:,i] = np.reshape(IMAGES[imi][r:r+sz, c:c+sz], sz*sz, 1)
 
     # Coefficient Inference
     if inference == 'lca':
@@ -47,7 +68,7 @@ def sparsenet(patch_dim=64, neurons=128, lambdav=0.1, eta=6.0, num_trials=3000, 
     elif inference == 'fista':
       ahat = fista.fista(I, Phi, lambdav, max_iterations=50)
     else:
-      print "Invalid inference option"
+      print("Invalid inference option")
       return
 
     # Calculate Residual Error
@@ -56,22 +77,34 @@ def sparsenet(patch_dim=64, neurons=128, lambdav=0.1, eta=6.0, num_trials=3000, 
     # Update Basis Functions
     dPhi = eta * (np.dot(R, ahat.T))
     Phi = Phi + dPhi
-    Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis = 0))))
+    Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis=0))))
 
     # Plot every 100 iterations
     if np.mod(t,100) == 0:
-      print "Iteration " + str(t)
-      side = np.sqrt(neurons)
+      print("Iteration " + str(t))
+      side = int(np.sqrt(neurons))
       image = np.zeros((sz*side+side,sz*side+side))
-      for i in range(side.astype(int)):
-        for j in range(side.astype(int)):
+      for i in range(side):
+        for j in range(side):
           patch = np.reshape(Phi[:,i*side+j],(sz,sz))
           patch = patch/np.max(np.abs(patch))
           image[i*sz+i:i*sz+sz+i,j*sz+j:j*sz+sz+j] = patch
 
-      plt.imshow(image, cmap=cm.Greys_r, interpolation="nearest")
+      if t == 0:
+        plt.ion()
+        fig = plt.figure()
+        im = plt.imshow(image, cmap=cm.Greys_r, interpolation="nearest")
+      else:
+        if plt.fignum_exists(fig.number):
+          im.set_data(image)
+        else:
+          print('Figure closed. Exiting')
+          return None
+      plt.pause(1e-6)
+      #  plt.draw()
 
   return Phi
+
 
 def sparsify(I, Phi, lambdav, eta=0.1, num_iterations=125):
   """
@@ -84,14 +117,14 @@ def sparsify(I, Phi, lambdav, eta=0.1, num_iterations=125):
   batch_size = np.shape(I)[1]
 
   (N, M) = np.shape(Phi)
-  sz = np.sqrt(N)
+  #  sz = np.sqrt(N)
 
   b = np.dot(Phi.T, I)
   G = np.dot(Phi.T, Phi) - np.eye(M)
 
   u = np.zeros((M,batch_size))
 
-  l = 0.5 * np.max(np.abs(b), axis = 0)
+  l = 0.5 * np.max(np.abs(b), axis=0)
   a = g(u, l)
 
   for t in range(num_iterations):
@@ -103,6 +136,7 @@ def sparsify(I, Phi, lambdav, eta=0.1, num_iterations=125):
 
   return a
 
+
 # g:  Hard threshold. L0 approximation
 def g(u,theta):
   """
@@ -110,6 +144,99 @@ def g(u,theta):
   u: coefficients
   theta: threshold value
   """
-  a = u;
+  a = u
   a[np.abs(a) < theta] = 0
   return a
+
+
+def preprocess(imgs, filt, lambdav=0.1):
+  """ Applies the necessary preprocessing described in the Olshausen Field paper
+  to turn natural images into ones acceptable by the algorithm
+
+  Inputs:
+    imgs - list(PIL.Image) : Raw images read from disk in PIL format.
+    filt - ndarray : Filter parametrized in the Fourier domain.
+
+  Returns:
+    list(ndarray) : list of numpy array of images after preprocessing done.
+  """
+  # The size of the fft
+  N = filt.shape[0]
+  # The max size of the image. Will be half the size of the FFT filter so that
+  # the periodization doesn't affect things.
+  M = N // 2
+  # Copy the list
+  imgs = [i for i in imgs]
+
+  for i in range(len(imgs)):
+    im = imgs[i]
+    x,y = im.size
+    if x > y:
+      n = (M, int(M*y/x))
+    else:
+      n = (int(M*x/y), M)
+    im.resize(n, Image.BILINEAR)
+
+    If = fft2(im, (N,N))
+    imagew = ifft2(If * filt).real[:y,:x]
+    imgs[i] = np.sqrt(lambdav) * imagew / np.std(imagew)
+
+  return imgs
+
+
+def get_filter():
+  """ Gets the denoising/whitening filter used by Olshausen & Field. Returns the
+  fourier representation of it.
+
+  Sets the cutoff frequency to be 0.8π
+  """
+  # Ensure that this large enough to handle all images
+  N = 1024
+  f0 = 0.4*N
+  fx, fy = np.meshgrid(np.arange(-N//2, N//2), np.arange(-N//2, N//2))
+  r = np.sqrt(fx**2 + fy**2)
+
+  # Calculate the lowpass filter (for noise removal)
+  lpf = np.exp(-(r/f0)**4)
+
+  # Calculate the whitener
+  whitener = r
+
+  # Filter
+  filt = fftshift(whitener * lpf)
+  return filt
+
+
+if __name__ == '__main__':
+  args = parser.parse_args()
+  if args.imgs is not None:
+    # Check that the folder exists
+    if not os.path.isdir(args.imgs):
+      print('Provided path not found')
+      exit()
+
+    # Check that the suffixes have a dot at the beginning
+    args.suffix = [s if s.beginswith('.') else '.'+s for s in args.suffix]
+
+    # Load in all the images in the given path
+    imgs = os.listdir(args.imgs)
+    imgs = [os.path.join(args.imgs, i) for i in imgs
+            if os.path.splitext(i)[1].lower() in args.suffix]
+    if len(imgs) == 0:
+      print('Could not find any images in the folder which matched '
+            'the provided suffixes')
+      exit()
+    imgs = [Image.open(i).convert('L') for i in imgs]
+
+    filt = get_filter()
+    IMAGES = preprocess(imgs, filt, args.lambdav)
+
+  else:
+    IMAGES = scipy.io.loadmat('./IMAGES.mat')
+    IMAGES = IMAGES['IMAGES']
+    IMAGES = np.transpose(IMAGES, (2,0,1))
+
+  sparsenet(IMAGES, patch_dim=args.size, neurons=128, lambdav=args.lambdav,
+            eta=6.0, num_trials=3000, batch_size=100, border=4,
+            inference=args.inference)
+  input('Press any key to exit')
